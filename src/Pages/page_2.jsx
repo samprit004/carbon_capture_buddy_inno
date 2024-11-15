ebd3e20ed8130d8204c124dbc5916ede10f22bc0
samprit <sampritdas2004@gmail.com>
2024-11-15 23:22:37 +0530

    actual_final

--- src/Pages/page_2.jsx
+++ src/Pages/page_2.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import ProductInput from '../components/ProductInput'
+import Nav from '../components/Nav'
 
 const page_2 = () => {
   return (
-    <div>
-      <h1>hello!!</h1>
-    </div>
+  <>
+  <Nav/>
+  <ProductInput/>
+  </>
   )
 }
 
